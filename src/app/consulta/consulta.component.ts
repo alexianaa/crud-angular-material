5d26081c33d1b92097b95743bf8b0832c12a4fa6
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-17 12:00:19 -0300

    adiciona tabela de consulta

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { Cliente } from '../cadastro/Cliente';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
+  listarClientes: Cliente[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'email', 'cpf', 'dataNascimento'];
 
+  constructor(private service: ClienteService){}
+
+  ngOnInit(){
+    this.listarClientes = this.service.pesquisarClientes('');
+    // console.log("listar", this.listarClientes);
+  }
 }
