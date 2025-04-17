2d6a78b462c97e4125234b83b85f8cbef94e3d7b
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-17 10:56:44 -0300

    adiciona formulario de cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {Cliente} from './Cliente';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cliente: Cliente = Cliente.newCliente();
 
+  salvar(){
+    console.log('Salvar cliente ', this.cliente );
+  }
 }
