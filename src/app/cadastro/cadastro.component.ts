8d0713d86e8d1f270bd8dc491bc3a66e2690f467
Alexia Cardoso <alexiasilvacardosoo@gmail.com>
2025-04-18 09:56:08 -0300

    adiciona mascara e mensagem de feedback

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
@@ -6,9 +6,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Cliente } from './Cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-cadastro',
@@ -20,6 +22,10 @@ import { ActivatedRoute, Router } from '@angular/router';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    NgxMaskDirective,
+  ],
+  providers: [
+    provideNgxMask(),
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
@@ -27,6 +33,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CadastroComponent implements OnInit {
   cliente: Cliente = Cliente.newCliente();
   atualizando: boolean = false;
+  snack: MatSnackBar = inject(MatSnackBar);
 
   constructor(
     private service: ClienteService,
@@ -54,9 +61,15 @@ export class CadastroComponent implements OnInit {
     if(!this.atualizando){
       this.service.salvar(this.cliente);
       this.cliente = Cliente.newCliente();
+      this.mostrarMensagem("Salvo com sucesso");
     }else{
       this.service.atualizar(this.cliente);
-      this.router.navigate(['/consulta'])
+      this.mostrarMensagem("Atualizado com sucesso");
+      this.router.navigate(['/consulta']);
     }
+  }
+
+  mostrarMensagem(mensagem: string){
+    this.snack.open(mensagem, "Ok");
   }
 }
--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -28,6 +29,7 @@ import { ClienteService } from '../cliente.service';
 })
 export class ConsultaComponent implements OnInit {
   listarClientes: Cliente[] = [];
+  snack: MatSnackBar = inject(MatSnackBar);
   nomeBusca: string = '';
   displayedColumns: string[] = ['id', 'nome', 'email', 'cpf', 'dataNascimento', 'editar','excluir'];
 
@@ -35,6 +37,10 @@ export class ConsultaComponent implements OnInit {
     private service: ClienteService,
     private router: Router
   ){}
+
+  mostrarMensagem(mensagem: string){
+    this.snack.open(mensagem, "Ok");
+  }
 
   ngOnInit(){
     this.listarClientes = this.service.pesquisarClientes('');
@@ -57,5 +63,6 @@ export class ConsultaComponent implements OnInit {
   excluir(cliente: Cliente){
     this.service.deletar(cliente);
     this.listarClientes = this.service.pesquisarClientes('');
+    this.mostrarMensagem("Item excluido com sucesso.");
   }
 }
